dd015a219a7088182eca31d76d9a76a12fb14fb0
Jeremy Winston <107045027+jeremywinst@users.noreply.github.com>
2023-10-31 20:02:17 +0900

    Add files via upload

---
+++ run2.py
@@ -0,0 +1,23 @@
+import os
+import time
+
+os.environ['CUDA_VISIBLE_DEVICES']='2'
+
+def ConvertSectoDay(n):
+    hour = n // 3600
+
+    n %= 3600
+    minutes = n // 60
+
+    n %= 60
+    seconds = n
+
+    print(hour, "hours", minutes, "minutes", int(seconds), "seconds")
+
+start_ALL = time.time()
+
+os.system('python kungfu_dqn.py --name left5 --r_left 5 --episode 750 --lr 0.0001 --decay 200 --end 0.1')
+
+elapsed_time_ALL = time.time() - start_ALL
+print('\n\n\nTRAINING FINISH!')
+print('TOTAL EXECUTION TIME: ', ConvertSectoDay(elapsed_time_ALL))
